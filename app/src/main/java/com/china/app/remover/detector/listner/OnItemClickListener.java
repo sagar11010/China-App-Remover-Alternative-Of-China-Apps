e42a190fe4b3a62fb1db9cd9ce217e71cc798c42
sagar11011 <91978639+sagar11011@users.noreply.github.com>
2021-10-06 00:31:14 +0530

    Update Method
    Update Method and some logic

--- app/src/main/java/com/china/app/remover/detector/listner/OnItemClickListener.java
+++ app/src/main/java/com/china/app/remover/detector/listner/OnItemClickListener.java
@@ -1,4 +1,5 @@
 package com.china.app.remover.detector.listner;
+//this is listner class for access click with position
 public interface OnItemClickListener {
     void onItemClick(int position);
 }
